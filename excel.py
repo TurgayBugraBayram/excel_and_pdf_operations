2adc1dd3a061d121df410390f0264729ae2d00f1
TurgayBugraBayram <turgaybugrabayram@gmail.com>
2024-07-08 19:31:39 +0300

    .

--- excel.py
+++ excel.py
@@ -9,32 +9,46 @@ sheet = wb.active
 
 columns = ['AK', 'IA', 'ID', 'IL', 'MI', 'MN', 'MT', 'ND', 'NV', 'OR', 'SD', 'UT', 'WA', 'WI']
 
-selected_state = input("Lütfen bir eyalet adı girin (örneğin 'AK'): ")
+selected_state = input("Please enter a state name (e.g., 'AK'): ")
 
 if selected_state not in columns:
-    print("Geçersiz eyalet adı. Lütfen doğru bir eyalet adı girin.")
+    print("Invalid state name. Please enter a valid state name.")
 else:
     col_idx = df.columns.get_loc(selected_state) + 1
 
     count_X_delete = 0
     form_data_X_delete = []
+    form_data_X = []
 
     for row in sheet.iter_rows(min_col=col_idx, max_col=col_idx, min_row=2):
         cell = row[0]
         cell_value = cell.value
-        if cell_value in ['X', 'x']:  
+
+        if cell_value in ['X', 'x']:
             if cell.font and cell.font.strike:
-                count_X_delete += 1
-                form_data_X_delete.append((cell.row, df.loc[cell.row - 2, ['Form number w/Edition date', 'Form Name']]))
+                if cell.row - 2 < len(df):
+                    count_X_delete += 1
+                    form_data_X_delete.append((cell.row, df.loc[cell.row - 2, ['Form number w/Edition date', 'Form Name']]))
+            else:
+                if cell.row - 2 < len(df):
+                    form_data_X.append((cell.row, df.loc[cell.row - 2, ['Form number w/Edition date', 'Form Name']]))
 
     count_X = df[selected_state].value_counts().get('X', 0) + df[selected_state].value_counts().get('x', 0)
 
-    print(f"\n{selected_state} sütununda {count_X_delete} tane üstü çizili 'X' veya 'x' var ve {count_X - count_X_delete} 'X' veya 'x' var.")
+    print(f"\nThere are {count_X_delete} crossed out 'X' or 'x' and {count_X - count_X_delete} non-crossed out 'X' or 'x' in the {selected_state} column.")
 
     if count_X_delete > 0:
-        print("\nÜstü çizili 'X' veya 'x' içeren formlar:")
+        print("\nForms with crossed out 'X' or 'x':")
         for row_num, data in form_data_X_delete:
             form_number, form_name = data['Form number w/Edition date'], data['Form Name']
             print(f"{row_num}: Form number w/Edition date: {form_number}, Form Name: {form_name}")
     else:
-        print(f"{selected_state} sütununda üstü çizili 'X' veya 'x' bulunamadı.")
+        print(f"No crossed out 'X' or 'x' found in the {selected_state} column.")
+
+    if (count_X - count_X_delete) > 0:
+        print("\nForms with non-crossed out 'X' or 'x':")
+        for row_num, data in form_data_X:
+            form_number, form_name = data['Form number w/Edition date'], data['Form Name']
+            print(f"{row_num}: Form number w/Edition date: {form_number}, Form Name: {form_name}")
+    else:
+        print(f"No non-crossed out 'X' or 'x' found in the {selected_state} column.")
