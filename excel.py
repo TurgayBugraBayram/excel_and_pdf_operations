04e7c893b24667e0c5855397fd708698e28b033c
TurgayBugraBayram <turgaybugrabayram@gmail.com>
2024-07-10 23:11:38 +0300

    .

--- excel.py
+++ excel.py
@@ -2,6 +2,7 @@ import pandas as pd
 from openpyxl import load_workbook
 
 file_path = 'PA-Forms_Tool.xlsx'
+update_file_path = 'update.xlsx'
 df = pd.read_excel(file_path, header=1)
 
 wb = load_workbook(file_path)
@@ -17,6 +18,7 @@ else:
     col_idx = df.columns.get_loc(selected_state) + 1
 
     count_X_delete = 0
+    count_X = 0
     form_data_X_delete = []
     form_data_X = []
 
@@ -31,11 +33,11 @@ else:
                     form_data_X_delete.append((cell.row, df.loc[cell.row - 2, ['Form number w/Edition date', 'Form Name']]))
             else:
                 if cell.row - 2 < len(df):
-                    form_data_X.append((cell.row, df.loc[cell.row - 2, ['Form number w/Edition date', 'Form Name']]))
+                    count_X += 1
+                    form_data_X.append((cell.row, df.loc[cell.row - 2, ['Form number w/Edition date', 'Form Name','GW Document Type']]))
 
-    count_X = df[selected_state].value_counts().get('X', 0) + df[selected_state].value_counts().get('x', 0)
 
-    print(f"\nThere are {count_X_delete} crossed out 'X' or 'x' and {count_X - count_X_delete} non-crossed out 'X' or 'x' in the {selected_state} column.")
+    print(f"\nThere are {count_X_delete} crossed out 'X' or 'x' and {count_X} non-crossed out 'X' or 'x' in the {selected_state} column.")
 
     if count_X_delete > 0:
         print("\nForms with crossed out 'X' or 'x':")
@@ -45,10 +47,15 @@ else:
     else:
         print(f"No crossed out 'X' or 'x' found in the {selected_state} column.")
 
-    if (count_X - count_X_delete) > 0:
+    if count_X > 0:
         print("\nForms with non-crossed out 'X' or 'x':")
         for row_num, data in form_data_X:
-            form_number, form_name = data['Form number w/Edition date'], data['Form Name']
-            print(f"{row_num}: Form number w/Edition date: {form_number}, Form Name: {form_name}")
+            form_number, form_name, form_GW = data['Form number w/Edition date'], data['Form Name'], data['GW Document Type']
+            print(f"{row_num}: Form number w/Edition date: {form_number}, Form Name: {form_name} {form_GW}")
     else:
         print(f"No non-crossed out 'X' or 'x' found in the {selected_state} column.")
+
+df_filtered =pd.DataFrame([data[1] for data in form_data_X])
+
+with pd.ExcelWriter(update_file_path,engine="openpyxl") as writer:
+    df_filtered.to_excel(writer, index=False)
